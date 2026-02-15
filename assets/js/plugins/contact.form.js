a49d5c5470d0860659a941083f56c0c3e9198309
Krishna Mangukiya <krishnamangukiya09@gmail.com>
2026-02-15 12:09:16 +0530

    created my portfolio

---
+++ assets/js/plugins/contact.form.js
@@ -0,0 +1,32 @@
+﻿/**
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Template : Krishna Personal Portfolio HTML Template
+ * Author : Krishna Mangukiya
+ *
+ * -----------------------------------------------------------------------------
+ *
+ **/
+
+(function ($) {
+    'use strict';
+
+    var form = $('#contact-form');
+    var formMessages = $('#form-messages');
+
+    $(form).submit(function (e) {
+        e.preventDefault();
+
+        // Form data serialize + phone field যুক্ত করা
+        var formData = $(form).serialize() + "&phone=" + $('#contact-phone').val();
+
+        // Simulate form submission for demo purposes
+        setTimeout(function () {
+            $(formMessages).removeClass('error').addClass('success').text('Thank you! Your message has been sent (Demo).');
+            $('#contact-name, #contact-email, #subject, #contact-message, #contact-phone').val('');
+        }, 1000);
+    });
+
+})(jQuery);
+
